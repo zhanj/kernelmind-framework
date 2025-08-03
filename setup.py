cfe5d1f74273bf16ff3fb068248a47a99478662f
Ethan <jiezhan1@gmail.com>
2025-08-03 11:16:50 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kernelmind",
-    version="0.1.0",
+    version="0.1.1",
     description="A minimalist thinking kernel for LLM agents",
     author="Ethan Zhan",
     author_email="jiezhan1@gmail.com",
